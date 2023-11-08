7271d43d99b1b00eefcaa7486bc3b155df7244d5
chaitanya7575 <i.chaitanyakumbhalkar@gmail.com>
2023-11-08 22:58:17 +0530

    first commit

---
+++ precache-manifest.148bdbb0d38a1e01c0e059e256ab8424.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d69149fb4d19117c8712",
+    "url": "/static/css/main.45bd6980.chunk.css"
+  },
+  {
+    "revision": "d69149fb4d19117c8712",
+    "url": "/static/js/main.d69149fb.chunk.js"
+  },
+  {
+    "revision": "e2723de9d6e9385cbe1e",
+    "url": "/static/js/1.e2723de9.chunk.js"
+  },
+  {
+    "revision": "46c1b2d47566d30e9ab9",
+    "url": "/static/js/2.46c1b2d4.chunk.js"
+  },
+  {
+    "revision": "56b2c7d1e2d5f244900f",
+    "url": "/static/js/runtime~main.56b2c7d1.js"
+  },
+  {
+    "revision": "0745b1937841fb5cbfbe39c3b1b37c7e",
+    "url": "/index.html"
+  }
+];
